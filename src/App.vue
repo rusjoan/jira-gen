3fb138169439985e3f57601913cca3a959798fa7
Evgeny Murashkin <rusjoan@ya.ru>
2018-04-20 12:05:03 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+    <div class="left">
+      <hot-table :root="root" :settings="hotSettings"></hot-table>
+    </div>
+    <div class="right">
+      <template v-for="(chunk, idx) in chunks">
+        <Page :tasks="chunk" :key="idx" />
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import Page from './components/Page'
+import HotTable from '@handsontable/vue'
+
+const tasks = require('../static/tasks')
+
+export default {
+  name: 'App',
+  data: () => ({
+    // tasks,
+    root: 'test-hot',
+    hotSettings: {
+      columns: [
+        {data: 'number', type: 'text', width: 80},
+        {data: 'title', type: 'text', width: 200},
+        {data: 'description', type: 'text', width: 400},
+        {data: 'price', type: 'text', width: 40},
+        {data: 'bug', type: 'numeric', width: 40},
+      ],
+      minRows: 1,
+      colHeaders: ['Number', 'Title', 'Description', 'Price', 'Bug?'],
+      width: '100%',
+      // data: tasks,
+      data: [
+        {
+          "number": "",
+          "title": "Заголовок",
+          "description": "Описание",
+          "price": "",
+          "bug": undefined,
+        },
+      ],
+    }
+  }),
+  computed: {
+    tasks () {
+      return this.hotSettings.data.filter(t => t.description && t.description.length > 0);
+    },
+    chunks () {
+      const chunkSize = 12;
+      return Array(Math.ceil(this.tasks.length / chunkSize)).fill(0).map((_, idx) => {
+        return this.tasks.slice(idx * chunkSize, (idx+1) * chunkSize);
+      })
+    }
+  },
+  components: {
+    Page,
+    HotTable,
+  },
+}
+</script>
+
+<style src="../node_modules/handsontable/dist/handsontable.full.css"></style>
+<style lang="scss" rel="stylesheet/sass">
+  body {
+    background: #fafafa;
+    padding: 1em;
+  }
+  #app {
+    display: flex;
+    .left, .right {
+      flex: 1 100%;
+      min-width: 400px;
+    }
+    .left {
+      overflow: hidden;
+    }
+    textarea {
+      height: 30em;
+    }
+  }
+  @media print {
+    body {
+      padding: 0;
+      -webkit-print-color-adjust: exact;
+
+    }
+    .left {
+      display: none;
+    }
+  }
+</style>
---
+++ src/components/Page.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="pdf-page">
+    <div class="task" :class="{'bug': task.bug === 1}" v-for="(task, idx) in tasks" :key="idx">
+      <div class="header">
+        <span class="number">{{ task.number }}</span>
+        <span class="title">{{ task.title }}</span>
+      </div>
+      <div class="body">
+        {{ task.description }}
+      </div>
+      <div class="footer">
+        SP: <b>{{ task.price || '[&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;]' }}</b>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tasks'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" rel="stylesheet/sass">
+  $border-color: rgba(0,0,0,.2);
+  .pdf-page {
+    size: A4;
+    width: 210mm;
+    height: 297mm;
+    box-shadow: 1px 1px 10px rgba(0,0,0,.4);
+    margin: 10px auto;
+    vertical-align: top;
+  }
+  @media print {
+    .pdf-page {
+      box-shadow: none;
+      margin: 0;
+    }
+  }
+  .task {
+    color: black;
+    width: 69mm;
+    height: 73mm;
+    font-size: 6mm;
+    border: .5mm solid $border-color;
+    display: inline-flex;
+    flex-direction: column;
+    font-family: "PT Sans";
+    margin: .5mm;
+    overflow: hidden;
+    position: relative;
+    background: #f5ff75 !important;
+    &.bug {
+      background: #ffc7c7 !important;
+    }
+    .header, .body, .footer {
+      padding: 1.6mm;
+    }
+    .header {
+      font-family: "PT Sans Caption";
+      border-bottom: .5mm solid $border-color;
+      font-size: 4mm;
+      line-height: 4mm;
+      .number {
+        font-weight: bold;
+        margin-right: 5mm;
+        display: block;
+        font-size: 10mm;
+        line-height: 8mm;
+        min-height: 6mm;
+      }
+    }
+    .body {
+      flex: 1;
+      font-size: 6mm;
+      line-height: 6mm;
+      text-overflow: ellipsis;
+      font-weight: 300;
+    }
+    .footer {
+      font-size: .6em;
+      position: absolute;
+      /*bottom: .5mm;*/
+      /*right: .5mm;*/
+      bottom: 0;
+      width: 100%;
+      height: 8mm;
+      /*font-weight: bold;*/
+      text-align: right;
+      background: linear-gradient(rgba(255,255,255,0), rgba(255,255,255,.8) 50%) !important;
+    }
+  }
+  @page {
+    size: A4;
+    margin: .2cm;
+  }
+</style>
